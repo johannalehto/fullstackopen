4658630dbdac0193a054c5dca2242e2ee0a628ba
Johanna Lehto <johannaeveliinalehto@gmail.com>
2022-09-28 10:29:51 +0300

    Part 1 Unicafe, task 1.9: Add conditional rendering to statistics

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,6 +1,15 @@
 import { useState } from 'react'
 
 const Statistics = ({good, neutral, bad}) => {
+  if (good === 0 && neutral === 0 && bad === 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p> No feedback given</p>
+      </div>
+    )
+  }
+
   return (
     <>
       <h1>statistics</h1>
