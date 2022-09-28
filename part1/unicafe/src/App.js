f1be0ae3f29bc83b6e7d0ffc3bee04035320b9eb
Johanna Lehto <johannaeveliinalehto@gmail.com>
2022-09-28 11:12:06 +0300

    Part 1 Unicafe, task 1.11: Add tbody-tag to the table to match the DOM tree

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -28,12 +28,14 @@ const Statistics = ({good, neutral, bad}) => {
   return (
     <>
       <table>
+        <tbody>
         <StatisticLine text="good" value={good} />
         <StatisticLine text="neutral" value={neutral} />
         <StatisticLine text="bad" value={bad} />
         <StatisticLine text="all" value={good + neutral + bad} />
         <StatisticLine text="average" value={((good - bad) / (good + neutral + bad)).toFixed(1)} />
         <StatisticLine text="positive" value={(good / (good + neutral + bad) * 100).toFixed(1) + " %"} />
+        </tbody>
       </table>
     </>
   )
