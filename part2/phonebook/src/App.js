caae62969862e626290fcca9da439dc259c720f6
Johanna Lehto <johannaeveliinalehto@gmail.com>
2022-10-12 12:39:23 +0300

    Part 2 Phonebook, task 2.6: Add new names from the input field to the persons -list

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,23 +2,40 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas'
+    }
   ]) 
   const [newName, setNewName] = useState('')
+
+  const handleName = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+    const newPerson = {
+      name: newName
+    }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input onChange={handleName} value={newName} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+      {persons.map(p => <li  key={p.name}>{p.name}</li>)}
+      </ul>
+     
     </div>
   )
 
