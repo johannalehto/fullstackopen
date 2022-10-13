a750358eb66e9348b86c0572e49b8dd3deb9c326
Johanna Lehto <johannaeveliinalehto@gmail.com>
2022-10-13 11:35:31 +0300

    Part 2 Phonebook, task 2.8: Add input field for number, add event handler for it, add number to the person-object

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,19 +2,27 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas'
+    { name: 'Arto Hellas',
+      number: '050-123 4567'
     }
   ]) 
+
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleName = (event) => {
     setNewName(event.target.value)
   }
 
+  const handleNumber = (event) => {
+    setNewNumber(event.target.value)
+  }
+
   const addName = (event) => {
     event.preventDefault()
     const newPerson = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
   
     const findPerson = persons.includes(persons.find((person) => person.name.toLowerCase() === newName.toLowerCase()))
@@ -34,12 +42,15 @@ const App = () => {
           name: <input onChange={handleName} value={newName} />
         </div>
         <div>
+          number: <input onChange={handleNumber} value={newNumber} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
-      {persons.map(p => <li  key={p.name}>{p.name}</li>)}
+      {persons.map(p => <li  key={p.name}>{p.name} {p.number}</li>)}
       </ul>
      
     </div>
