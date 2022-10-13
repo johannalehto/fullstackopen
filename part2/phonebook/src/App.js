9d4038db4ce7537879faf1a57949baed04dacc71
Johanna Lehto <johannaeveliinalehto@gmail.com>
2022-10-13 12:42:27 +0300

    Part 2 Phonebook, task 2.9: Add input for filtering names, add event handler for it, change display of the persons

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,13 +2,15 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '050-123 4567'
-    }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
   const handleName = (event) => {
     setNewName(event.target.value)
@@ -18,15 +20,19 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilter = (event) => {
+    setNewFilter(event.target.value)
+  }
+
   const addName = (event) => {
     event.preventDefault()
     const newPerson = {
       name: newName,
       number: newNumber
     }
-  
+
     const findPerson = persons.includes(persons.find((person) => person.name.toLowerCase() === newName.toLowerCase()))
-   
+  
     findPerson 
     ? alert(`${newName} is already added to the phonebook`)
     : setPersons(persons.concat(newPerson)) 
@@ -34,9 +40,18 @@ const App = () => {
     setNewName('')
   }
 
+  const viewPersons = (newFilter === "")
+    ? persons
+    : persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase(), 0))
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <div>
+        filter shown with <input onChange={handleFilter} value={newFilter} />
+      </div>
+
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: <input onChange={handleName} value={newName} />
@@ -50,7 +65,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-      {persons.map(p => <li  key={p.name}>{p.name} {p.number}</li>)}
+      {viewPersons.map(p => <li  key={p.name}>{p.name} {p.number}</li>)}
       </ul>
      
     </div>
