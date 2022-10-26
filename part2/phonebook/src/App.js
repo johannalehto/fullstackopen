977ef87f63586a228496ce0adea26ebc0f5ea551
Johanna Lehto <johannaeveliinalehto@gmail.com>
2022-10-26 13:36:39 +0300

    Part 2 Phonebook, task 2.16: Create a separate module for the server communication

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import personService from './services/persons'
 
 
 const App = () => {
@@ -13,10 +13,9 @@ const App = () => {
   const [newFilter, setNewFilter] = useState('')
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
+    personService.getAll()
+    .then(initialPersons => {
+      setPersons(initialPersons)
       })
   }, [])
 
@@ -43,10 +42,10 @@ const App = () => {
   
     findPerson 
     ? alert(`${newName} is already added to the phonebook`)
-    : axios
-      .post('http://localhost:3001/persons', newPerson)
-      .then(response => {
-        setPersons(persons.concat(newPerson)) 
+    : personService
+      .create(newPerson)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson)) 
         setNewName('')
       })
 
